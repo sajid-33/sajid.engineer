a3e7da0b00e637b0934f6fd20df3107cdd9494ff
Sajid IN <insajidinsajid33@gmail.com>
2025-06-01 20:42:22 +0530

    Update myprogram.py

--- myprogram.py
+++ myprogram.py
@@ -1,28 +1,22 @@
-# guess_number.py
+# tree_names.py
 
-import random
+def print_tree_names():
+    trees = [
+        "Oak",
+        "Maple",
+        "Pine",
+        "Birch",
+        "Cedar",
+        "Spruce",
+        "Willow",
+        "Redwood",
+        "Palm",
+        "Baobab"
+    ]
 
-def guess_the_number():
-    number_to_guess = random.randint(1, 10)
-    tries = 0
-
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 10.")
-
-    while True:
-        try:
-            guess = int(input("Take a guess: "))
-            tries += 1
-
-            if guess < number_to_guess:
-                print("Too low! Try again.")
-            elif guess > number_to_guess:
-                print("Too high! Try again.")
-            else:
-                print(f"Congratulations! You guessed it in {tries} tries.")
-                break
-        except ValueError:
-            print("Please enter a valid number.")
+    print("List of 10 Tree Names:")
+    for tree in trees:
+        print(f"- {tree}")
 
 if __name__ == "__main__":
-    guess_the_number()
+    print_tree_names()
