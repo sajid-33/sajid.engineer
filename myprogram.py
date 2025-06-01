68502dcb882c12c26fb363f1db30c040c025e5f8
Sajid IN <insajidinsajid33@gmail.com>
2025-06-01 20:34:42 +0530

    Update myprogram.py

--- myprogram.py
+++ myprogram.py
@@ -1,2 +1,13 @@
-def say_hello():
-    return "Hello, World!"
+# print_alphabets.py
+
+import string
+
+def print_alphabets():
+    print("Uppercase Letters:")
+    print(string.ascii_uppercase)
+
+    print("\nLowercase Letters:")
+    print(string.ascii_lowercase)
+
+# Call the function
+print_alphabets()
